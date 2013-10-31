cc537406b29add579dd13fa87a6e2e02f6bb3e18
kph1g10 <kph1g10@ecs.soton.ac.uk>
2013-10-31 14:39:11 +0000

    Create php class Car
    Create php class called Car with attributes of the car table in the
    database.

--- classes/car.class.php
+++ classes/car.class.php
@@ -1,3 +1,13 @@
 <?php
-
+	class Car{
+		public  $id;
+		public  $manufacturer_id;
+		public  $colour_id;
+		public  $model;
+		public  $description;
+		public  $price;
+		public  $regNumber;
+		public  $regDate;
+	}
+?>
 // car class for part 1
