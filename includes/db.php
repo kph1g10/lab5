0d3fd742db2fd2519024cc91ab642b5d8a30dc0d
kph1g10 <kph1g10@ecs.soton.ac.uk>
2013-10-31 15:22:17 +0000

    If statement in case of failure
    An error message will now be printed if there is a connection error to
    the database.

--- includes/db.php
+++ includes/db.php
@@ -1,4 +1,7 @@
 <?php
 	$db = new mysqli("comp2203.ecs.soton.ac.uk","comp2203-lab5","WL2133r1lM7lzp1706Ne","comp2203-lab5");
+	if($db->connect_errno){
+		echo "Failed to connect to mysql; " . $db->connect_error;
+	}
 	
 ?>
